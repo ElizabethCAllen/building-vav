003a0a975b6a3258e3e4236e41e073feb2927600
Elizabeth <elizabethallen36@gmail.com>
2019-05-06 19:08:37 -0700

    working on js functions

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+function formatName(firstName, lastName){
+    console.log(firstName);
+    console.log(lastName);
+    console.log(lastName + '' + firstName);
+    return lastName + '' + firstName;
+    
+}
+var userFirstName = 'Liz';
+var userLastName = 'Allen';
+
+formatName(userFirstName, userLastName);
