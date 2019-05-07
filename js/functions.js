97c71d556ec8e8e95de259fd27f2b3bfa9e5db9b
Elizabeth <elizabethallen36@gmail.com>
2019-05-06 19:43:13 -0700

    I hate functions

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,33 @@
-function formatName(firstName, lastName){
-    console.log(firstName);
-    console.log(lastName);
-    console.log(lastName + '' + firstName);
+function formattedName(firstName, lastName){
+    var userFirstName = 'Liz';
+    var userLastName = 'Allen';
+    // console.log(firstName);
+    // console.log(lastName);
+    // console.log(lastName + '' + firstName);
     return lastName + '' + firstName;
-    
+    console.log(localStorage.getItem('userFullName'));
+    if(!localStorage.getItem('userFullName') ){
+         localStorage.setItem('userFullName', formattedName) ;
+    }
+    return formattedName;
 }
 var userFirstName = 'Liz';
 var userLastName = 'Allen';
 
-formatName(userFirstName, userLastName);
+// formatName(userFirstName, userLastName);
+
+// function expression
+var creatUserObj = function(firstName, lastName) {
+    var userObj = {
+        name: formattedName(firstName, lastName)
+        // age  : null 
+    };
+    return userObj;
+} (userFirstName, userLastName);
+// console.log(createUserObj);
+
+
+// constructor way
+// car concatinate = new function('x', 'y', 'return x + y' );
+// console.log( concatinate ('liz', 'allen'));
+
