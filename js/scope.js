3759c5f05472ae1e563af0d12353b772c7a69c0f
Elizabeth <elizabethallen36@gmail.com>
2019-05-06 20:20:15 -0700

    nested scopes

---
+++ js/scope.js
@@ -0,0 +1,30 @@
+// global scope
+var globalVar = 'hello world';
+
+function sayHello(){
+    console.log(globalVar);
+    var functVar = 'I am scoped inside a function';
+    console.log(functVar);
+}
+sayHello();
+console.log(globalVar);
+
+
+function secondFunction(){
+    console.log(functVar);
+    
+}
+
+function outerFunction(){
+    const outer= 'I\'m defined in outerFunction';
+    console.log(outer);
+    function innerFunction(){
+        const innerFunction = 'I\'m defined inside innerFunction';
+        console.log(inner);
+        
+    }    
+    innerFunction();
+    console.log(inner);
+    
+}
+outerFunction();
